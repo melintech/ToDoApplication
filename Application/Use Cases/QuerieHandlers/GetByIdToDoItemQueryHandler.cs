384753e34ec4208c5008ac72db02b4c9f507c83b
rocadenis <119231290+rocadenis@users.noreply.github.com>
2024-10-28 15:59:20 +0200

    update

---
+++ Application/Use Cases/QuerieHandlers/GetByIdToDoItemQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.DTOs;
+using Application.Use_Cases.Queries;
+using AutoMapper;
+using Domain.Repositories;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Use_Cases.QueryHandlers
+{
+    public class GetByIdToDoItemQueryHandler : IRequestHandler<GetByIdToDoItemQuery, ToDoItemDto>
+    {
+        private readonly IToDoItemRepository repository;
+        private readonly IMapper mapper;
+
+        public GetByIdToDoItemQueryHandler(IToDoItemRepository repository, IMapper mapper)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+        }
+
+        public async Task<ToDoItemDto> Handle(GetByIdToDoItemQuery request, CancellationToken cancellationToken)
+        {
+            var item = await repository.GetByIdAsync(request.Id);
+            return item != null ? mapper.Map<ToDoItemDto>(item) : null;
+        }
+    }
+}
---
+++ Application/Use Cases/Queries/GetByIdToDoItemQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Application.DTOs;
+using System;
+
+namespace Application.Use_Cases.Queries
+{
+    public class GetByIdToDoItemQuery : IRequest<ToDoItemDto>
+    {
+        public Guid Id { get; set; }
+
+        public GetByIdToDoItemQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
